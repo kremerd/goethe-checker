e519923046fe901265772c78a8bd67f19b5d3c3c
Dominik Kremer <dominik.kremer@gmx.de>
2022-11-23 22:00:20 +0100

    Implement automatic refresh query

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,14 @@
+import { AsyncPipe, JsonPipe, NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import {
+  filter,
+  interval,
+  map,
+  Observable,
+  shareReplay,
+  startWith,
+  switchMap,
+} from 'rxjs';
 import { GoetheService } from './goethe.service';
 
 @Component({
@@ -6,13 +16,31 @@ import { GoetheService } from './goethe.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
+  imports: [AsyncPipe, NgIf, JsonPipe],
 })
 export class AppComponent implements OnInit {
-  title = 'goethe-checker';
+  readonly REFRESH_INTERVAL = 60;
+  timeUntilRefresh$!: Observable<number>;
+  lastResponse$!: Observable<unknown>;
+  lastResponseSuccessful$!: Observable<boolean>;
 
   constructor(private readonly goetheService: GoetheService) {}
 
   ngOnInit(): void {
-    //this.goetheService.query().subscribe(console.log);
+    this.timeUntilRefresh$ = interval(1000).pipe(
+      map((n) => this.REFRESH_INTERVAL - (n % this.REFRESH_INTERVAL) - 1),
+      shareReplay(1)
+    );
+
+    this.lastResponse$ = this.timeUntilRefresh$.pipe(
+      filter((timeUntilRefresh) => timeUntilRefresh === 0),
+      startWith(0),
+      switchMap(() => this.goetheService.query()),
+      shareReplay(1)
+    );
+
+    this.lastResponseSuccessful$ = this.lastResponse$.pipe(
+      map((response) => Array.isArray(response) && response.length > 0)
+    );
   }
 }
--- src/app/goethe.service.ts
+++ src/app/goethe.service.ts
@@ -1,15 +1,28 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Injectable, isDevMode } from '@angular/core';
+import { catchError, Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GoetheService {
-  private readonly url = 'https://goethelb.com/api/dates-all?category_id=11';
+  private readonly goetheUrl =
+    'https://goethelb.com/api/dates-all?category_id=11';
+  private readonly meowFactsUrl = 'https://meowfacts.herokuapp.com/';
+
   constructor(private readonly http: HttpClient) {}
 
   query(): Observable<any> {
-    return this.http.get(this.url);
+    return this.http
+      .get(this.getUrl())
+      .pipe(catchError(() => of('Fehler beim Abfragen der Termine')));
+  }
+
+  private getUrl(): string {
+    if (isDevMode()) {
+      return this.meowFactsUrl;
+    } else {
+      return this.goetheUrl;
+    }
   }
 }
